3b52ed45b513f5f5bc149d8c8ad4d66759232e2a
Davis Klevgard <davisjklevgard@gmail.com>
2020-01-30 12:01:18 -0600

    Worked to correctly describe the program

---
+++ BuildingJavaPrograms/Chapter1/Hello.java
@@ -0,0 +1,11 @@
+package Chapter1;
+/*
+this class is named Hello which can been seen and used by anyone
+ */
+public class Hello {
+    //this procedural-style method is named main can be seen by all returns nothing
+    public static void main(String[] args){
+        //this statement prints to the console what is in the "" and ends the line
+        System.out.println("Hello, World!");
+    }
+}
