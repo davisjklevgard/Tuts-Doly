c9acf788ae27a31c8eab00ea92adbf66fdf2c677
Davis Klevgard <davisjklevgard@gmail.com>
2020-03-22 16:36:57 -0500

    Committing solution

--- ProjectSolutions/projectsolutions/FirstUniqueLetter.java
+++ ProjectSolutions/projectsolutions/FirstUniqueLetter.java
@@ -6,25 +6,34 @@ This Program is to help find the first unique letter in a string.
 public class FirstUniqueLetter{
     private static int TOTAL_CHARACTERS = 256;
 
-    private int[] trackerOfCountsOfCharacters = new int [TOTAL_CHARACTERS];
+    public static void main(String[] args) {
+        finding("Whole");
+    }
 
-    public char finding(String string){
-        char answer = (char) string.length();
-        for (int i = 0; i <= string.length(); i++){
+    private static int[] trackerOfCountsOfCharacters = new int [TOTAL_CHARACTERS];
+
+    public static char finding(String string){
+        char answer = 0;
+        for (int i = 0; i < string.length(); i++){
             funWithASCIIandCHAR(string.charAt(i));
         }
-        for (int i = 0; i <= TOTAL_CHARACTERS; i++){
+        for (int i = 0; i < TOTAL_CHARACTERS; i++){
             if(trackerOfCountsOfCharacters[i] == 1) {
                 char curranswer = (char) i;
-                if (string.indexOf(curranswer) < string.indexOf(answer)) {
+                if (string.indexOf(curranswer) < string.length()) {
                     answer = curranswer;
+
+                }
+                if (answer == 1){
+                    return answer;
                 }
             }
         }
+        
         return answer;
     }
 
-    public void funWithASCIIandCHAR(char letter) {
+    public static void funWithASCIIandCHAR(char letter) {
         trackerOfCountsOfCharacters[letter]= trackerOfCountsOfCharacters[letter]++;
     }
 }
