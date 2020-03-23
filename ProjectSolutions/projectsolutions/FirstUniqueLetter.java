88aa994f8301f0e4ddb2735e78921a0fee016563
Davis Klevgard <davisjklevgard@gmail.com>
2020-03-23 09:28:51 -0500

    Problem is in the if statement

--- ProjectSolutions/projectsolutions/FirstUniqueLetter.java
+++ ProjectSolutions/projectsolutions/FirstUniqueLetter.java
@@ -7,25 +7,37 @@ public class FirstUniqueLetter{
     private static int TOTAL_CHARACTERS = 256;
 
     public static void main(String[] args) {
-        finding("Whole");
+
+        System.out.println(finding("aardvark"));
+        System.out.println("Hello");
     }
 
     private static int[] trackerOfCountsOfCharacters = new int [TOTAL_CHARACTERS];
 
     public static char finding(String string){
         char answer = 0;
+        System.out.println(answer);
         for (int i = 0; i < string.length(); i++){
+            //System.out.println("a");
             funWithASCIIandCHAR(string.charAt(i));
+            //System.out.println(funWithASCIIandCHAR(string.charAt(i)));
         }
         for (int i = 0; i < TOTAL_CHARACTERS; i++){
+            //System.out.println("a");
             if(trackerOfCountsOfCharacters[i] == 1) {
+                //System.out.println("a");
                 char curranswer = (char) i;
+                System.out.println(curranswer);
                 if (string.indexOf(curranswer) < string.length()) {
                     answer = curranswer;
+                    System.out.println(answer);
 
                 }
+                System.out.println("a");
                 if (answer == 1){
+
                     return answer;
+
                 }
             }
         }
