41f9d1db02047afe376fa902f9dc1b396938c23a
Davis Klevgard <davisjklevgard@gmail.com>
2020-04-05 14:18:15 -0500

    Adding to Website project

---
+++ Website/Landlord/Buildings.java
@@ -0,0 +1,43 @@
+package Landlord;
+import java.util.*;
+
+public class Buildings {
+    public static void main(String[] args){
+    }
+    public class newBuilding{
+        Scanner console = new Scanner(System.in);
+        public void units(){
+            System.out.println("how many Units are in the building?");
+            int u = console.nextInt();
+            int [] unitsInBuilding = new int[u];
+        }
+    }
+
+    public class building{
+        public int units(){
+
+        }
+
+        public String tenants(){
+
+        }
+        public String complaints(){
+
+        }
+
+        public class mortgage{
+            public int due(){
+
+            }
+            public int paid(){
+
+            }
+        }
+    }
+
+    public class tenants{
+        public String allTenants() {
+
+        }
+    }
+}
